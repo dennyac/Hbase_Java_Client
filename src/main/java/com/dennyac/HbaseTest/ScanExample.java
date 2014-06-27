408fc8d8ee8d30e31fb3c387ab2210d860e298c4
Denny Abraham Cheriyan <dennyac@gmail.com>
2014-06-27 09:24:41 -0700

    Added ScanExample

--- src/main/java/com/dennyac/HbaseTest/ScanExample.java
+++ src/main/java/com/dennyac/HbaseTest/ScanExample.java
@@ -3,41 +3,42 @@ package com.dennyac.HbaseTest;
 //cc GetExample Example application retrieving data from HBase
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
 //import util.HBaseHelper;
+
 
 import java.io.IOException;
 
-public class GetExample {
+public class ScanExample {
 
 	public static void main(String[] args) throws IOException {
-		// vv GetExample
+
 		System.out.println("Initializing");
 		Configuration conf = HBaseConfiguration.create();
+		
+		System.out.println("Created Configuration");
 		conf.set("hbase.zookeeper.quorum", "172.31.2.26");
 		conf.set("hbase.zookeeper.property.clientPort", "2181");
 
 		System.out.println("Configuration created");
-		HTable table = new HTable(conf, "denny_test"); 
-
-
-		System.out.println("HTable object create");
-		Get get = new Get(Bytes.toBytes("r1")); 
-
-		System.out.println("Creating get object");
-		get.addColumn(Bytes.toBytes("d"), Bytes.toBytes("c1")); 
-
-		System.out.println("Adding column to get object");
-		Result result = table.get(get); 
-
-		System.out.println("Assigning byte array");
-		byte[] val = result.getValue(Bytes.toBytes("d"), Bytes.toBytes("c1")); 
-
-		System.out.println("Value: " + Bytes.toString(val)); 
-
+		HTable table = new HTable(conf, "msd_year");
+		System.out.println("HTable instantiated");
+		
+		Scan scan = new Scan();
+		System.out.println("Scan object instantiated");
+		ResultScanner scanner = table.getScanner(scan);
+		System.out.println("ResultScanner object instantiated");
+		for(Result res: scanner){
+			System.out.println(res);
+			System.out.println("Next iteration");
+		}
+		
+		scanner.close();
+		System.out.println("Closed scanner");
 		table.close();
+		System.out.println("Closed table");
 	}
 }
